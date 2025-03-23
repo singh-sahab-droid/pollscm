0a1c063737b876f6761ecfb53cebabd96096c602
singh-sahab-droid <mukeshsinghjsrn95@gmail.com>
2025-03-23 20:52:52 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Pipeline with Poll SCM!");
+        System.out.println("Hello from Jenkins Pipeline with Poll SCM with Mukesh singh!");
     }
 }
